75731d35ea308a49b15a081ffe260fcb0843f5b5
ColtonZP <39172201+ColtonZP@users.noreply.github.com>
2020-04-13 20:38:35 -0700

    init commit

---
+++ quickid.js
@@ -0,0 +1,16 @@
+function quickId() {
+  const date = Date.now().toString().substring(3);
+  let chars = '';
+  for (let i = 0; i < 5; i++) {
+    const ranChar = Math.floor(Math.random() * Math.floor(26));
+    let char = String.fromCharCode(ranChar + 65);
+    const toLower = Math.random() > .5;
+    if (toLower) {
+      char = char.toLowerCase();
+    }
+    chars = chars + char;
+  }
+  return `${chars}-${date}`;
+}
+
+module.exports.quickid = quickid;
---
+++ readme.md
@@ -0,0 +1,14 @@
+# quickid
+
+Effortlessly generate a unique 10 character id based on time and random character.
+
+# Install
+
+`npm install quickid --save`
+
+# Use
+```
+import { quickiId } from 'quickid';
+
+const id = quickId();
+```
